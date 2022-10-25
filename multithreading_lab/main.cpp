d74b4bcbc1831ba65bd4f56a2e5ee40da0605ddd
psteeep <psteeep@users.noreply.github.com>
2022-10-25 04:03:37 +0300

    add multithreading

--- multithreading_lab/main.cpp
+++ multithreading_lab/main.cpp
@@ -16,38 +16,63 @@ void compare(string fake_file_name) {
 
 	int sum_line = 0;
 
+
+
+	ofstream result_file("result.txt", std::ios_base::app);
+
+
 	if (original_file.is_open() && fake_file.is_open()) {
 		while (getline(original_file, line) && getline(fake_file, line2))
 		{
 			sum_line++;
 
 			if (line != line2) {
-				cout << sum_line;
+				result_file << sum_line << endl;
+				cout << " - " << sum_line << " - ";
 			}
 			else
 				continue;
-			cout << endl;
+			
 		}
 	}
+	//result_file << this_thread::get_id() << endl;
+	//cout << this_thread::get_id() << endl;
+	original_file.close();
+	fake_file.close();
+	result_file.close();
 };
 
 
 int main() {
 	
-	/*
+	
 	thread th1(compare, "fake_file.txt");
+	th1.join();
+	cout << endl;
 	thread th2(compare, "fake2_file.txt");
+	th2.join();
+	cout << endl;
 	thread th3(compare, "fake3_file.txt");
+	th3.join();
+	cout << endl;
 	thread th4(compare, "original2_file.txt");
+	th4.join();
 
+	/*
 	th1.join();
 	th2.join();
 	th3.join();
 	th4.join();
 	*/
-
+	
+	/*
+	compare("fake_file.txt");
+	cout << endl;
+	compare("fake2_file.txt");
+	cout << endl;
+	compare("fake3_file.txt");
+	cout << endl;
 	compare("original2_file.txt");
-
-
+	*/
 	return 0;
 }
